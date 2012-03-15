af58ab933e09ad2fba71976b8139ed531fb78d75
Jocelyn Batton <batton.jocelyn@gmail.com>
2012-03-15 16:57:23 +0100

    tentative de mise en place du service REST

--- src/valtechBiblio/appli/ValtechBiblioActivity.java
+++ src/valtechBiblio/appli/ValtechBiblioActivity.java
@@ -1,35 +1,18 @@
 package valtechBiblio.appli;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.List;
-
 import valtechBiblio.Model.Book;
 import valtechBiblio.dao.ILibrary;
-import valtechBiblio.dao.MockLibrary;
 import valtechBiblio.dao.RemoteLibrary;
 import android.app.Activity;
-import android.app.ListActivity;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class ValtechBiblioActivity extends Activity 
@@ -43,24 +26,13 @@ public class ValtechBiblioActivity extends Activity
     {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.main);
-    	String[] urls = new String[1];
-		urls[0] = "http://i714.photobucket.com/albums/ww145/2shay78/book.png";
 		imView = (ImageView)findViewById(R.id.imview);
-		new DownloadImageTask().execute("http://ecx.images-amazon.com/images/I/51PpZM%2BcuXL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA160_.jpg");
-    	
-    	
-    	
-    	// initialisation du DAO pour récupérer les données
-    	
-    	
-    	//List<Book> books2 = new MockLibrary().findAllBooks();
-    	
-    	
+		new DownloadBooksTask().execute();
     }
     
-    private class DownloadImageTask extends AsyncTask<String, Void, List<Book>> 
+    private class DownloadBooksTask extends AsyncTask<Void, Void, List<Book>> 
     {
-        protected List<Book> doInBackground(String... urls) 
+        protected List<Book> doInBackground(Void... unused) 
         {
         	ILibrary library = new RemoteLibrary();
         	List<Book> books = library.findAllBooks();
@@ -71,7 +43,6 @@ public class ValtechBiblioActivity extends Activity
         {
         	final ArrayAdapter<Book> adapter2 = new CustomAdapter(ValtechBiblioActivity.this,result);
         	ListView listView = (ListView) findViewById(R.id.mylist);
-    		// Assign adapter to ListView
     		listView.setAdapter(adapter2);
     		listView.setOnItemClickListener(new OnItemClickListener() {
     			public void onItemClick(AdapterView<?> parent, View view,
@@ -82,40 +53,5 @@ public class ValtechBiblioActivity extends Activity
     			}
     		});
         }
-        
-        
-        Bitmap downloadFile(String fileUrl)
-        {
-        	Bitmap bmImg = null;
-            URL myFileUrl =null;          
-            try 
-            {
-                 myFileUrl= new URL(fileUrl);
-            } catch (MalformedURLException e) {
-                 // TODO Auto-generated catch block
-                 e.printStackTrace();
-            }
-            try 
-            {
-                 HttpURLConnection conn= (HttpURLConnection)myFileUrl.openConnection();       
-                 conn.setDoInput(true);
-                 conn.connect();
-                 InputStream is = conn.getInputStream();
-                 bmImg = BitmapFactory.decodeStream(is);
-                 //conn.disconnect();
-                 //imView.setImageBitmap(bmImg);
-                 return bmImg;
-            } 
-            catch (IOException e) 
-            {
-                 // TODO Auto-generated catch block
-                 e.printStackTrace();
-            }
-			return bmImg;
-       }
-        
-        
-       
-    }
-    
+    }  
 }
--- src/valtechBiblio/dao/RemoteLibrary.java
+++ src/valtechBiblio/dao/RemoteLibrary.java
@@ -1,13 +1,27 @@
 package valtechBiblio.dao;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 
 import valtechBiblio.Model.Book;
 
@@ -24,6 +38,64 @@ public class RemoteLibrary implements ILibrary
 	public List<Book> findAllBooks() 
 	{
 		List<Book> tempBooks = mockLibrary.findAllBooks();
+		HttpClient httpclient = new DefaultHttpClient();
+		
+		HttpGet httpget = new HttpGet("http://localhost:5984/"); 
+		//HttpGet httpget = new HttpGet("http://www.google.fr");
+		
+		HttpResponse response;
+        try {
+            response = httpclient.execute(httpget);
+            // Examine the response status
+            Log.i("Praeda",response.getStatusLine().toString());
+ 
+            // Get hold of the response entity
+            HttpEntity entity = response.getEntity();
+            // If the response does not enclose an entity, there is no need
+            // to worry about connection release
+ 
+            if (entity != null) {
+ 
+                // A Simple JSON Response Read
+                InputStream instream = entity.getContent();
+                String result= convertStreamToString(instream);
+                Log.i("Praeda",result);
+ 
+                // A Simple JSONObject Creation
+                JSONObject json=new JSONObject(result);
+                Log.i("Praeda","<jsonobject>\n"+json.toString()+"\n</jsonobject>");
+ 
+                // A Simple JSONObject Parsing
+                JSONArray nameArray=json.names();
+                JSONArray valArray=json.toJSONArray(nameArray);
+                for(int i=0;i<valArray.length();i++)
+                {
+                    Log.i("Praeda","<jsonname"+i+">\n"+nameArray.getString(i)+"\n</jsonname"+i+">\n"
+                            +"<jsonvalue"+i+">\n"+valArray.getString(i)+"\n</jsonvalue"+i+">");
+                }
+ 
+                // A Simple JSONObject Value Pushing
+                json.put("sample key", "sample value");
+                Log.i("Praeda","<jsonobject>\n"+json.toString()+"\n</jsonobject>");
+ 
+                // Closing the input stream will trigger connection release
+                instream.close();
+            }
+ 
+ 
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+		
+		
+		
 		for(Book book : tempBooks)
 		{
 			book.setPicture(downloadFile(book.getUrlPicture()));
@@ -87,7 +159,32 @@ public class RemoteLibrary implements ILibrary
    }
 	
 	
-	
+	private static String convertStreamToString(InputStream is) {
+        /*
+         * To convert the InputStream to String we use the BufferedReader.readLine()
+         * method. We iterate until the BufferedReader return null which means
+         * there's no more data to read. Each line will appended to a StringBuilder
+         * and returned as String.
+         */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+ 
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
     
     
 	
